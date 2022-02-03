6bdf681eaecd3bd2a4a11c8febd6e374261f5622
qkrwjdrb <miolionpjg@gmail.com>
2022-02-03 18:03:41 +0900

    Influxdb테스트폼 생성

---
+++ DB_TestForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InfluxDB.Client.Writes;
+using InfluxDB.Client;
+
+namespace DB_TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            influxdbTest();
+        }
+        private void influxdbTest()
+        {
+            var influxDBClient = InfluxDBClientFactory.Create("http://localhost:8086", "t7HUaHSBPVheH6QK351z1qpwuCX_rhq2vsX_-V7kqagcu7cNfRgR-wL0gzM6csRXIE0W8_r3I4AWETwmfSNRVQ==");
+
+            var point = PointData.Measurement("test_measurement")
+                               .Field("value", (float)88);
+            influxDBClient.GetWriteApi().WritePoint("testdb", "saltanb", point);
+
+        }
+    }
+}
